d700009aa8a98c4928f595a2477d26affdd5ebc9
Ewen Bouquet <ewen.bouquet@lunatech.fr>
2025-05-08 19:34:35 +0200

    Add RAG ingestion/retriever

---
+++ src/main/java/fr/ewen/questions/QuestionRagIngestion.java
@@ -0,0 +1,44 @@
+package fr.ewen.questions;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import io.quarkus.logging.Log;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import lombok.extern.jbosslog.JBossLog;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static dev.langchain4j.data.document.splitter.DocumentSplitters.recursive;
+
+@ApplicationScoped
+public class QuestionRagIngestion {
+
+    public void ingest(
+        @Observes StartupEvent ev,
+        EmbeddingStore<TextSegment> store,
+        EmbeddingModel model,
+        @ConfigProperty(name = "ai.rag.location") Path path
+    ) {
+        Log.infof("Starting rag ingestion...");
+
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+            .embeddingStore(store)
+            .embeddingModel(model)
+            .documentSplitter(recursive(1024, 0))
+            .build();
+
+        List<Document> documents = FileSystemDocumentLoader.loadDocumentsRecursively(path);
+        documents.forEach(document -> Log.infof("Ingesting: %s", document));
+        ingestor.ingest(documents);
+
+        Log.info("Ending rag ingestion...");
+    }
+}
---
+++ src/main/java/fr/ewen/questions/QuestionRagRetriever.java
@@ -0,0 +1,55 @@
+package fr.ewen.questions;
+
+import dev.langchain4j.data.message.*;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.Content;
+import dev.langchain4j.rag.content.injector.ContentInjector;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import io.quarkus.logging.Log;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+
+import java.util.List;
+
+public class QuestionRagRetriever {
+
+    @Produces
+    @ApplicationScoped
+    public RetrievalAugmentor retrieve(
+        EmbeddingStore<TextSegment> store,
+        EmbeddingModel model
+    ) {
+        Log.infof("Starting rag retrieval...");
+
+        EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+            .embeddingModel(model)
+            .embeddingStore(store)
+            .maxResults(3) // Large segments
+            .build();
+
+        DefaultRetrievalAugmentor contentAugmentor = DefaultRetrievalAugmentor.builder()
+                .contentRetriever(contentRetriever)
+                .contentInjector((contents, message) -> {
+                    StringBuilder prompt = new StringBuilder(
+                        switch (message) {
+                            case null -> "";
+                            case UserMessage u -> u.singleText();
+                        }
+                    );
+                    prompt.append("\nPlease, only use the following information:\n");
+                    contents.forEach(content -> prompt.append("- ").append(content.textSegment().text()).append("\n"));
+
+                    Log.infof("Using prompt '%s'", prompt.toString());
+                    return new UserMessage(prompt.toString());
+                })
+                .build();
+
+        Log.infof("Ending rag retrieval...");
+
+        return contentAugmentor;
+    }
+}
